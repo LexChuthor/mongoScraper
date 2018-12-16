3f6e5f7076a33d768e4738b1dd7587e98c8db866
Lex Chuthor <psc9qb@gmail.com>
2018-12-15 19:54:21 -0500

    automatically scrape on page load

--- public/app.js
+++ public/app.js
@@ -1,4 +1,9 @@
 // Grab the articles as a json
+$(document).ready(function(){
+  $.get("/scrape", function(){
+    console.log("scraping completed");
+  });
+})
 $.getJSON("api/articles", function (data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
